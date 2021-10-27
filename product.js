a012dfc0097e8e8dc452b31d0ba298f8b7abbed9
Hoang Lam <hoanglam55766@gmail.com>
2021-10-27 22:00:38 +0700

    upload foodlist and image

---
+++ product.js
@@ -0,0 +1,168 @@
+const Foods= [
+    {
+
+        ID:'01',
+        image:'/image/banhxeo.jpg',
+        name:'Bánh xèo',
+        price:50000,
+        GIaGoc:40000,
+        Soluong:2,
+        Dangco:0,
+        STT:'',
+    },
+    {
+
+        ID:'02',
+        image:'/image/Banhmibokho.jpg',
+        name:'Bánh mì bò kho',
+        price:30000,
+        GIaGoc:25000,
+        Soluong:2,
+        Dangco:8,
+        STT:'',
+    },    
+    {
+
+        ID:'03',
+        image:'/image/Bunbohue.jpg',
+        name:'Bún bò huế',
+        price:50000,
+        GIaGoc:40000,
+        Soluong:2,
+        Dangco:6,
+        STT:'',
+    },    
+    {
+
+        ID:'04',
+        image:'/image/chaolong.jpg',
+        name:'Cháo lòng',
+        price:30000,
+        GIaGoc:25000,
+        Soluong:2,
+        Dangco:8,
+        STT:'',
+    },   
+    {
+
+        ID:'05',
+        image:'/image/comtam.jpg',
+        name:'Cơm tấm',
+        price:25000,
+        GIaGoc:20000,
+        Soluong:2,
+        Dangco:6,
+        STT:'',
+    },    
+    {
+
+        ID:'06',
+        image:'/image/Ganuong.jpg',
+        name:'Gà nướng cơm lam',
+        price:150000,
+        GIaGoc:100000,
+        Soluong:2,
+        Dangco:1,
+        STT:'',
+    },    
+    {
+
+        ID:'07',
+        image:'/image/Laucaduoi.jpg',
+        name:'Lẫu cá đuối',
+        price:90000,
+        GIaGoc:60000,
+        Soluong:2,
+        Dangco:3,
+        STT:'',
+    },    
+    {
+
+        ID:'08',
+        image:'/image/mixao.jpg',
+        name:'Mì xào',
+        price:20000,
+        GIaGoc:15000,
+        Soluong:2,
+        Dangco:1,
+        STT:'',
+    },    
+    {
+
+        ID:'09',
+        image:'/image/nemnuong.jpg',
+        name:'Nem nướng',
+        price:30000,
+        GIaGoc:20000,
+        Soluong:2,
+        Dangco:5,
+        STT:'',
+    },    
+    {
+
+        ID:'10',
+        image:'/image/Pho.png',
+        name:'Phở',
+        price:50000,
+        GIaGoc:40000,
+        Soluong:2,
+        Dangco:0,
+        STT:'',
+    },    
+    {
+
+        ID:'11',
+        image:'/image/nuocsuoi.jpg',
+        name:'Nước suối Aquafina',
+        price:6000,
+        GIaGoc:3000,
+        Soluong:5,
+        Dangco:20,
+        STT:'',
+    },    
+    {
+
+        ID:'12',
+        image:'/image/coca.jpg',
+        name:'Coca',
+        price:10000,
+        GIaGoc:5000,
+        Soluong:2,
+        Dangco:12,
+        STT:'',
+    },    
+    {
+
+        ID:'13',
+        image:'/image/nuoccam.jpg',
+        name:'Cam vắt',
+        price:12000,
+        GIaGoc:6000,
+        Soluong:2,
+        Dangco:5,
+        STT:'',
+    },    
+    {
+
+        ID:'14',
+        image:'/image/tratac.png',
+        name:'Trà tắc',
+        price:10000,
+        GIaGoc:5000,
+        Soluong:2,
+        Dangco:6,
+        STT:'',
+    },    
+    {
+
+        ID:'15',
+        image:'/image/sodachanh.jpg',
+        name:'Soda Chanh',
+        price:15000,
+        GIaGoc:8000,
+        Soluong:2,
+        Dangco:4,
+        STT:'',
+    },    
+] 
+export default Foods
