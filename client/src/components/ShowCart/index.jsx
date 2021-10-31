956f21f9454d718bf5504580abbd55a1042d69c8
hoangkui <hoangkui2001@gmail.com>
2021-10-31 21:42:47 +0700

    hoangkui implement cart

--- client/src/components/ShowCart/index.jsx
+++ client/src/components/ShowCart/index.jsx
@@ -1,61 +1,59 @@
-import React from 'react'
-import "./index.css"
+import React from "react";
+import "./index.css";
+import ProductInCart from "../ProductInCart";
 const ShowCart = () => {
-    return (
-        <>
-            <div className="cart-icon">
-            <i class="fas fa-shopping-cart"></i>
-            </div>
-            <div className="cart">
-                <div className="cart-header">
-                    <p className="cart-header-title">
-            <i class="fas fa-shopping-cart"></i>
+
+    const handleClickCart=()=>{
+        document.querySelector(".cart").classList.add("display");
+    }
+    const handleClickClose=()=>{
+        document.querySelector(".cart").classList.remove("display");
+        
+    }
+  return (
+    <>
+      <div className="cart-icon"
+      onClick={handleClickCart}
+      >
+    
+        <i className="fas fa-shopping-cart"></i>
+      </div>
+      <div className="cart">
+          <div className="cart-close"
+          onClick={handleClickClose}
+          >
+          <i class="fas fa-times-circle"></i>
+          </div>
+        <div className="cart-header">
+          <p className="cart-header-title">
+            <i className="fas fa-shopping-cart"></i>
             Your Cart (6)
-                    </p>
-                    <button className="cart-header-button">DINE IN</button>
-                </div>
-                <ul className="cart-list">
-                    <li className="cart-item">
-
-                        <img src="https://cdn.tgdd.vn/2020/08/CookProduct/3-1200x676-2.jpg" alt="" className="cart-item-img-src" />
-                        <div className="cart-item-content">
-                            <p className="cart-item-content-title">
-                                1. Grilled squid satay
-                            </p>
-                            <div className="cart-item-content-add-list">
-                                <div className="cart-item-content-add-item">
-                                <p className="cart-item-content-add-item-title">
-                                    Đồ ăn thêm: Rau thơm
-                                </p>
-                                <p className="cart-item-content-add-item-price">kr 5.50</p>
-
-                                </div>
-                                <div className="cart-item-content-add-item">
-                                <p className="cart-item-content-add-item-title">
-                                    Đồ ăn thêm: Rau thơm
-                                </p>
-                                <p className="cart-item-content-add-item-price">kr 5.50</p>
-
-                                </div>
-                            </div>
-                            <div className="cart-item-content-bottom">
-                                <div className="cart-item-content-bottom-button">
-                                    - 1 +
-                                </div>
-                                <div className="cart-item-content-bottom-price">
-                                <p className="cart-item-content-bottom-price-origin">Kr 123.00</p>
-                                <p className="cart-item-content-bottom-price-tax">
-                                    Incl.tax 10% = Kr 12.30
-                                </p>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
+          </p>
+          <button className="cart-header-button">DINE IN</button>
+        </div>
+        <ul className="cart-list">
+          <ProductInCart />
+          <ProductInCart />
+          <ProductInCart />
+          <ProductInCart />
+          <ProductInCart />
+          <ProductInCart />
+        </ul>
+        <div className="cart-payment">
+          <div className="cart-payment-total">
+            <p className="cart-payment-total-text">Total</p>
+            <div className="cart-payment-total-price">
+              <p className="cart-payment-total-origin">Kr 123.00</p>
+              <p className="cart-payment-total-tax">
+                (Incl.tax 10% = Kr 12.30)
+              </p>
             </div>
+          </div>
+          <button className="cart-payment-button">PAYMENT</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default ShowCart
+export default ShowCart;
