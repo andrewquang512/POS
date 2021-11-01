6d572576ae2b05a3d61054bbf0cf4f991e659a1c
andrewquang <andrew.viquang@gmail.com>
2021-11-01 20:51:13 +0700

    init connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
+import connectDB from './config/db.js'
+const PORT = 5000
 
+connectDB()
 const app = express()
-const port = 5000
 
 app.use(express.json())
 
 app.get('/', (req, res) => res.send('Hello all!'))
 
-app.listen(port, () =>
-  console.log(`Server is running at http://localhost:${port}`)
+app.listen(PORT, () =>
+  console.log(`Server is running at http://localhost:${PORT}`)
 )
