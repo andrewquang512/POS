048da423cdff2d7cc210a82b313585311e6b9961
andrewquang <andrew.viquang@gmail.com>
2021-11-07 01:28:00 +0700

    Revert "Add files via upload"
    This reverts commit e1d2fc0ebed388f0da5ae603146918fc3473ed2b.

--- client/src/ShopContext.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-// import Foods from "./Foods";
-export default React.createContext({
-    cart: [],
-    addProductToCart: product => {},
-    removeProductFromCart: productId => {}
-});
