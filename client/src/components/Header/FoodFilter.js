161df70307135231d629cc55281f392cdc995968
thuongle2210 <67187155+thuongle2210@users.noreply.github.com>
2021-11-06 11:10:01 +0700

    Add files via upload
    create logic for filter

--- client/src/components/Header/FoodFilter.js
+++ client/src/components/Header/FoodFilter.js
@@ -1,22 +1,32 @@
-import React,{useRef, useState} from 'react'
+import React from 'react'
+import Foods from '../Foods'
+import Menu from '../Menu';
+const TypeProducts={
+  1: 'https://thescranline.com/wp-content/uploads/2021/03/Vanilla-Cupcakes.jpg',
+  2: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Plateau_van_zeevruchten.jpg/1200px-Plateau_van_zeevruchten.jpg',
+  3: 'https://cordis.europa.eu/docs/results/images/2020-02/415400.jpg',
+  4: 'https://cf.shopee.vn/file/f6a87bd170eab0d27850ca866bcbc66e',
+  5: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmN2HCg7kPZPYTpVRJv8DaSnioDEJtsl6bQQ&usqp=CAU',
+};
 
-const FoodFilter = ({name,id,selected,handleOneSelected}) => {
-  var className="filter-item"
-  if (selected===id) className="filter-item filter-item-seleted"
-  const filterRef=useRef()
-  const handleClickFilter=(id)=>{
-    handleOneSelected(id)
 
+function ShowType(){
+  <Menu
+  typeId = {0}
+  Showall = {false}
+  />
+}
+
+function FoodFilter({name, id, x_value}){
+  function getMenu(){
+    x_value(id);
+    
   }
-    return (
+  return (
         <>
-               
-          <li
-          ref={filterRef}
-          onClick={()=>handleClickFilter(id)}
-          className={className}>
+          <li className="filter-item" onClick={getMenu}>
             <img
-              src="https://thumbs.dreamstime.com/b/yummy-vanilla-cupcake-white-background-vertical-14882157.jpg"
+              src={TypeProducts[id]}
               alt=""
               className="filter-item-img"
             />
