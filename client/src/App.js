774788b1dac522fa273afee524b1b2dda7ec079c
andrewquang <andrew.viquang@gmail.com>
2021-11-07 01:30:17 +0700

    Revert "Add files via upload"
    This reverts commit 81266a4995ef475ad61c76f36c337ab5994abdd4.

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,11 @@ import Menu from './components/Menu';
 import './index.css';
 import ShowCart from "./components/ShowCart"
 import GlobalState from './components/GlobalState';
-import Filter from './components/Header/Filter';
-import {useState} from "react"
-import React from 'react';
 function App() {
-  const [idType,setIdType]=useState(-1)
-  function ChangeForIdType(id){
-    setIdType(id);
-  }
   return (
-
     <GlobalState>
       <ShowCart/>
-      <Filter x={ChangeForIdType}></Filter>
-      <Menu typeId = {idType} Showall = {false}/>
+      <Menu />
     </GlobalState>
   );
 }
