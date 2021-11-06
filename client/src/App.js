2d887a67ab05c81c67d73b1cddc68d1b6377599b
thuongle2210 <67187155+thuongle2210@users.noreply.github.com>
2021-11-06 21:54:06 +0700

    Add files via upload
    update index.css
    update structure of App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,23 @@ import Menu from './components/Menu';
 import './index.css';
 import ShowCart from "./components/ShowCart"
 import GlobalState from './components/GlobalState';
+import { useState } from 'react';
 import Filter from './components/Header/Filter';
-import {useState} from "react"
-import React from 'react';
+import TypeProducts from './components/TypeProducts';
+import ShowInforLine from './components/ShowInforLine';
 function App() {
   const [idType,setIdType]=useState(-1)
   function ChangeForIdType(id){
     setIdType(id);
   }
   return (
+      <GlobalState>
+        <ShowCart/>
+        <Filter x={ChangeForIdType}></Filter>
+        <ShowInforLine typeId={idType}/>
+        <Menu typeId = {idType}/>
 
-    <GlobalState>
-      <ShowCart/>
-      <Filter x={ChangeForIdType}></Filter>
-      <Menu typeId = {idType} Showall = {false}/>
-    </GlobalState>
+      </GlobalState>
   );
 }
 
