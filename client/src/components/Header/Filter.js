4486c4b99ec5b6f97abd0c224d4862d3a765d4ee
hoangkui <hoangkui2001@gmail.com>
2021-11-06 09:41:45 +0700

    animation slider

--- client/src/components/Header/Filter.js
+++ client/src/components/Header/Filter.js
@@ -1,24 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useRef,useEffect } from "react";
 import FoodFilter from "./FoodFilter";
+import Slider from "react-slick";
 import { Types } from "./Types";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 console.log(Types);
 const Filter = () => {
+  const [selected, setSelected] = useState(-1);
+  const ref = useRef({});
+
+  const next = () => {
+    ref.current.slickNext();
+  };
+
+  const previous = () => {
+    ref.current.slickPrev();
+  };
+
+  const settings = {
+    className: "section-outstanding__slider hoangkui-css-filter",
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: true,
+    rows: 1,
+    centered: "true",
+    responsive: [
+      {
+        breakpoint: 1198,
+        settings: {
+          className: "stick-list-cc",
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          rows: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          rows: 1,
+        },
+      },
+    ],
+  };
+  const handleOneSelected = (id) => {
+    setSelected(id)
+  };
+
+  useEffect(() => {
+    
+    return () => {
+      console.log("pre",selected);
+    }
+  }, [selected])
+
   const [types, setTypes] = useState(Types);
-  const handleNext = () => {
-    const newTypes = [...types];
-    const firtType=newTypes.shift()
-    newTypes.push(firtType)
-    setTypes(newTypes)
-    
-  };
-  const handleBack = () => {
-    
-    const newTypes = [...types];
-    const firtType=newTypes.pop()
-    newTypes.unshift(firtType)
-    setTypes(newTypes)
-    
-  };
+
   return (
     <>
       <div className="backtohome">
@@ -26,27 +65,30 @@ const Filter = () => {
         <h3 className="backtohome-title">Back to Home</h3>
       </div>
 
-      <div className="filter-food">
-        <div className="button-filter button-back">
-          <div onClick={handleBack} class="arrow-left-food arrow-food"></div>
-        </div>
+      {/* <div className="filter-food"> */}
+      <div className="button-filter button-back">
+        <div onClick={previous} class="arrow-left-food arrow-food"></div>
+      </div>
 
-        <ul className="filter-list">
-          {types.map((typeFood, index) => {
-            if (index < 8)
-              return (
-                <FoodFilter
-                  key={typeFood.typeId}
-                  name={typeFood.typeName}
-                  id={typeFood.typeId}
-                />
-              );
-          })}
-        </ul>
+      {/* <ul className="filter-list"> */}
+      <Slider ref={ref} {...settings}>
+        <FoodFilter handleOneSelected={handleOneSelected} selected={selected} key={-1} name="All" id={-1} />
+        {types.map((typeFood, index) => {
+          return (
+            <FoodFilter
+            handleOneSelected={handleOneSelected}
+            selected={selected}
+              // className={}
+              key={typeFood.typeId}
+              name={typeFood.typeName}
+              id={typeFood.typeId}
+            />
+          );
+        })}
+      </Slider>
 
-        <div className="button-filter button-next">
-          <div onClick={handleNext}  class="arrow-right-food arrow-food"></div>
-        </div>
+      <div className="button-filter button-next">
+        <div onClick={next} class="arrow-right-food arrow-food"></div>
       </div>
     </>
   );
--- client/src/components/Header/FoodFilter.js
+++ client/src/components/Header/FoodFilter.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React,{useRef, useState} from 'react'
 
-const FoodFilter = ({name,id}) => {
+const FoodFilter = ({name,id,selected,handleOneSelected}) => {
+  var className="filter-item"
+  if (selected===id) className="filter-item filter-item-seleted"
+  const filterRef=useRef()
+  const handleClickFilter=(id)=>{
+    handleOneSelected(id)
+
+  }
     return (
         <>
                
-          <li className="filter-item">
+          <li
+          ref={filterRef}
+          onClick={()=>handleClickFilter(id)}
+          className={className}>
             <img
               src="https://thumbs.dreamstime.com/b/yummy-vanilla-cupcake-white-background-vertical-14882157.jpg"
               alt=""
