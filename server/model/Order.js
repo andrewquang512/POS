54f7a8fee4c8223298c381a486eb9a987409a9e6
andrewquang <andrew.viquang@gmail.com>
2021-11-15 17:00:52 +0700

    update order model, route, action

---
+++ server/model/Order.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const OrderSchema = new Schema({
+  OrderItems: [
+    {
+      name: { type: String, required: true },
+      count: { type: Number, required: true },
+      img: { type: String, required: true },
+      price: { type: Number, required: true },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Product',
+      },
+    },
+  ],
+  paymentMethod: {
+    type: String,
+    required: true,
+    default: 'Cash',
+  },
+  paymentResult: {
+    id: { type: String },
+    status: { type: String },
+    update_time: { type: String },
+    email_address: { type: String },
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  isPaid: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  paidAt: {
+    type: Date,
+  },
+  isDelivered: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  deliveredAt: {
+    type: Date,
+  },
+})
+
+module.exports = mongoose.model('Order', OrderSchema)
---
+++ server/routers/orderRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const orderController = require('../controllers/orderController.js')
+
+router.route('/').post(addOrderItems).get(getOrders)
+router.route('/myorders').get(getMyOrders)
+router.route('/:id').get(getOrderById)
+router.route('/:id/pay').put(updateOrderToPaid)
+router.route('/:id/deliver').put(updateOrderToDelivered)
+
+module.exports = router
