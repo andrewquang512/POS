892279ba821eb1bc1d30feeda7ed4b3c82a78fa8
andrewquang <andrew.viquang@gmail.com>
2021-11-15 17:41:24 +0700

    update order

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
-import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
 import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_FAIL,
+  ORDER_DETAILS_FAIL,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_REQUEST,
+  ORDER_PAY_FAIL,
   ORDER_PAY_SUCCESS,
   ORDER_PAY_REQUEST,
+  ORDER_LIST_FAIL,
   ORDER_LIST_SUCCESS,
   ORDER_LIST_REQUEST,
+  ORDER_DELIVER_FAIL,
   ORDER_DELIVER_SUCCESS,
   ORDER_DELIVER_REQUEST,
 } from '../constants/orderConstants'
@@ -25,17 +29,15 @@ export const createOrder = (order) => async (dispatch, getState) => {
       type: ORDER_CREATE_SUCCESS,
       payload: data,
     })
-    dispatch({
-      type: CART_CLEAR_ITEMS,
-      payload: data,
-    })
-    localStorage.removeItem('cartItems')
   } catch (error) {
     const message =
       error.response && error.response.data.message
         ? error.response.data.message
         : error.message
-    console.log(message)
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload: message,
+    })
   }
 }
 
@@ -56,7 +58,10 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
       error.response && error.response.data.message
         ? error.response.data.message
         : error.message
-    console.log(message)
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload: message,
+    })
   }
 }
 
@@ -81,7 +86,10 @@ export const payOrder =
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message
-      console.log(message)
+      dispatch({
+        type: ORDER_PAY_FAIL,
+        payload: message,
+      })
     }
   }
 
@@ -102,7 +110,10 @@ export const deliverOrder = (order) => async (dispatch, getState) => {
       error.response && error.response.data.message
         ? error.response.data.message
         : error.message
-    console.log(message)
+    dispatch({
+      type: ORDER_DELIVER_FAIL,
+      payload: message,
+    })
   }
 }
 
@@ -123,6 +134,9 @@ export const listOrders = () => async (dispatch, getState) => {
       error.response && error.response.data.message
         ? error.response.data.message
         : error.message
-    console.log(message)
+    dispatch({
+      type: ORDER_LIST_FAIL,
+      payload: message,
+    })
   }
 }
