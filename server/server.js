d1d7e21f2000552e8fbe335047a61ec90e36df76
andrewquang512 <andrew.viquang@gmail.com>
2022-03-18 23:52:27 +0700

    change for deploy

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server is running at http://localhost:${PORT}`)
 );
