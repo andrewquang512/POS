5c3f2cd61ee348adf25ef7ebf6dc01819b39dc08
andrewquang512 <andrew.viquang@gmail.com>
2022-03-18 23:09:34 +0700

    change for deploy

--- server/server.js
+++ server/server.js
@@ -1,30 +1,38 @@
-const express = require("express");
-const cors = require("cors");
-const connectDB = require("./config/db");
-const productRouter = require("./routers/product");
-const typeProductRouter = require("./routers/typeProduct");
-const orderRoutes = require("./routers/orderRoutes");
-const revenueRoutes = require("./routers/revenueRouter");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const connectDB = require('./config/db');
+const productRouter = require('./routers/product');
+const typeProductRouter = require('./routers/typeProduct');
+const orderRoutes = require('./routers/orderRoutes');
+const revenueRoutes = require('./routers/revenueRouter');
 // const fileUpload = require("express-fileupload");
 const app = express();
 
-app.use("/profile", productRouter);
+app.use('/profile', productRouter);
 
 connectDB();
 app.use(cors());
 app.use(express.json());
 // app.use(fileUpload());
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 // app.use(fileupload());
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static('uploads'));
 
-app.use("/api/product/", productRouter);
-app.use("/api/typeproduct/", typeProductRouter);
-app.use("/api/orders", orderRoutes);
-app.use("/revenue", revenueRoutes);
+app.use('/api/product/', productRouter);
+app.use('/api/typeproduct/', typeProductRouter);
+app.use('/api/orders', orderRoutes);
+app.use('/revenue', revenueRoutes);
 // app.get("/", (req, res) => res.send("Hello all!"));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  });
+}
 
 const PORT = 5000;
 app.listen(PORT, () =>
